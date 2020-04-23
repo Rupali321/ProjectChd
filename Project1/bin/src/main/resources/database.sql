86268739702d2f29ad81844eb7aabed080c86b80
hp <hp@DESKTOP-VIPN8G9>
2020-04-23 19:08:23 +0530

    method added

---
+++ Project1/bin/src/main/resources/database.sql
@@ -0,0 +1,17 @@
+DROP DATABASE if exists Employee_DB;
+CREATE DATABASE Employee_DB;
+USE  Employee_DB;
+
+
+create table Employee(employee_id integer(6) primary key, name varchar(25) not null, mobile_no varchar(10) not null); 
+
+CREATE SEQUENCE hibernate_sequence;
+
+insert into Employee values(2001,'Markel',8765476548);
+insert into Employee values(2002,'Chris', 9696459375);
+insert into Employee values(2003,'James', 8957216439);
+insert into Employee values(2004,'Alex',9140814428);
+insert into Employee values(2005,'Bernard',7941123214);
+insert into Employee values(2006,'Michel',8941123214);
+
+select * from Employee;
